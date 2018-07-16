a43b0e495254a5cb16aabc1eafe943428e9df542
chrollo1 <40929644+chrollo1@users.noreply.github.com>
2018-07-16 00:52:09 -0400

    fix

---
+++ ReverseStringWithStack.java
@@ -0,0 +1,31 @@
+package ds;
+
+import java.util.Stack;
+
+public class ReverseStringWithStack {
+
+	public static void reverse(StringBuffer s) {
+		
+		int n = s.length();
+		Stack<Character> st = new Stack<>();
+
+		// push chars of string 
+		for (int i = 0; i < n; i++) {
+			st.push(s.charAt(i));
+		}
+
+		for (int i = 0; i < n; i++) { 
+			char ch = st.pop();
+			s.setCharAt(i,ch);
+		}
+	} 
+
+	public static void main(String args[]) {
+
+		StringBuffer sb = new StringBuffer("Hello");
+		reverse(sb);
+
+		System.out.println("reversed is: " + sb);
+	}
+	
+}
